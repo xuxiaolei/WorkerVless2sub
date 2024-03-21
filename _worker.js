a551d32feb4beb1d9d5d46e083ccc7e17b31510a
leo <mumupudding@qq.com>
2024-03-21 10:00:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -35,7 +35,8 @@ let CMproxyIPs = [
 ];
 let BotToken ='';
 let ChatID =''; 
-let proxyhosts = [//本地代理域名池
+//本地代理域名池
+let proxyhosts = [
 	//'ppfv2tl9veojd-maillazy.pages.dev',
 ];
 let proxyhostsURL = 'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/proxyhosts';//在线代理域名池URL
